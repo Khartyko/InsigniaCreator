d6a444c312356b609a880145cbffc2919032c31f
Damien Bevins <damien.bevins@outlook.com>
2023-08-13 04:21:57 -0500

    Disable a warning

--- Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Entity/Atlas.cs
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Entity/Atlas.cs
@@ -1,5 +1,8 @@
 using Khartyko.InsigniaCreator.Library.Data;
 using Khartyko.InsigniaCreator.Library.Utility.Helpers;
+
+#pragma warning disable CS0659
+
 namespace Khartyko.InsigniaCreator.Library.Entity;
 
 public class Atlas : IIdBearer
