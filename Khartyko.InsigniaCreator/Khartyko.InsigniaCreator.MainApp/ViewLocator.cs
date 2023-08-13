155c994ee6aef1c56f7ee73b93c2a5d79801f9ae
Damien Bevins <damien.bevins@outlook.com>
2023-08-13 00:27:42 -0500

    Add Tests for the ViewLocator. Update the implementation to check for nulls
    So this originally was getting to be a headache, but it turns out I had to use the "AssemblyQualifiedName" instead of the "FullName", that is used by default by Avalonia UI.

--- Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.MainApp/ViewLocator.cs
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.MainApp/ViewLocator.cs
@@ -7,9 +7,14 @@ namespace Khartyko.InsigniaCreator.MainApp;
 
 public class ViewLocator : IDataTemplate
 {
-    public Control Build(object data)
+    public Control Build(object? data)
     {
-        var name = data.GetType().FullName!.Replace("ViewModel", "View");
+        if (data is null)
+        {
+            return new TextBlock { Text = "Null object passed." };
+        }
+        
+        string name = data.GetType().AssemblyQualifiedName!.Replace("ViewModel", "View");
         var type = Type.GetType(name);
 
         if (type != null)
@@ -20,7 +25,7 @@ public class ViewLocator : IDataTemplate
         return new TextBlock { Text = "Not Found: " + name };
     }
 
-    public bool Match(object data)
+    public bool Match(object? data)
     {
         return data is ViewModelBase;
     }
