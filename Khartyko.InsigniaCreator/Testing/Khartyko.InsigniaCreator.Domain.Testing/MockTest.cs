e92e5ff108fc8119dba4d0a61c8051c2879b249c
Damien Bevins <damien.bevins@outlook.com>
2023-08-26 13:20:57 -0500

    Remove temporary code for constructing a unit test

--- Khartyko.InsigniaCreator/Testing/Khartyko.InsigniaCreator.Domain.Testing/MockTest.cs
+++ Khartyko.InsigniaCreator/Testing/Khartyko.InsigniaCreator.Domain.Testing/MockTest.cs
@@ -6,34 +6,8 @@ namespace Khartyko.InsigniaCreator.Domain.Testing;
 
 public class MockTest
 {
-    private string _name = "Kjotvyko";
-
-    public string Name
-    {
-        get
-        {
-            ReflectionMetadata metadata = ReflectionHelper.GetCallerMetadata();
-
-            MethodInfo? methodInfo = GetType()
-                .GetMethod(metadata.MethodBase.Name);
-
-             return _name;
-        }
-        set
-        {
-            ReflectionMetadata metadata = ReflectionHelper.GetCallerMetadata();
-
-            MethodInfo? methodInfo = GetType()
-                .GetMethod(metadata.MethodBase.Name);
-
-            _name = value;
-        }
-    }
-
     [Fact]
     public void PassingTest()
     {
-        Name = "Khartyko";
-        _ = Name;
     }
 }
