fabf6cee38462d21bbd1f51676dc629b6c705994
Damien Bevins <damien.bevins@outlook.com>
2023-08-28 02:06:09 -0500

    Split the static Vector2 methods into its own class, and add Vector2 documentation.

--- Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Data/Vector2.cs
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Data/Vector2.cs
@@ -7,14 +7,17 @@ using Khartyko.InsigniaCreator.Library.Utility.Helpers;
 
 namespace Khartyko.InsigniaCreator.Library.Data;
 
-public class Vector2
+public partial class Vector2
 {
-    public static readonly Vector2 Zero = new(0, 0);
-    public static readonly Vector2 One = new(1, 1);
-
     private double _x;
     private double _y;
 
+    /// <summary>
+    /// Accesses the first value of a Vector2 instance.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentException' if an attempt to assign it to NaN, PositiveInfinity, or NegativeInfinity.
+    /// </remarks>
     public double X
     {
         get => _x;
@@ -26,6 +29,12 @@ public class Vector2
         }
     }
 
+    /// <summary>
+    /// Accesses the second value of a Vector2 instance.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentException' if an attempt to assign it to NaN, PositiveInfinity, or NegativeInfinity.
+    /// </remarks>
     public double Y
     {
         get => _y;
@@ -37,8 +46,20 @@ public class Vector2
         }
     }
 
+    /// <summary>
+    /// Calculates the length of a vector given the current X and Y values.
+    /// </summary>
     public double Length => MathHelper.Sqrt(X * X + Y * Y);
 
+    /// <summary>
+    /// Gets a double using the index operator.
+    /// </summary>
+    /// <remarks>
+    /// The following exceptions can be thrown:
+    /// - ArgumentOutOfRangeException: If the index used is not 0 or 1.
+    /// - ArgumentException: If the value is NaN, PositiveInfinity, or NegativeInfinity.
+    /// </remarks>
+    /// <param name="idx">The index of the value</param>
     public double this[int idx]
     {
         get
@@ -64,11 +85,26 @@ public class Vector2
         }
     }
 
+    /// <summary>
+    /// Creates a Vector2 instance with both values being the double value passed in.
+    /// </summary>
+    /// <remarks>
+    /// This can throw an 'ArgumentException' if the value is NaN, PositiveInfinity, or NegativeInfinity.
+    /// </remarks>
+    /// <param name="value">The value of both 'X' and 'Y'.</param>
     public Vector2(double value)
         : this(value, value)
     {
     }
 
+    /// <summary>
+    /// Creates a Vector2 instance with 2 double values.
+    /// </summary>
+    /// <remarks>
+    /// This can throw an 'ArgumentException' if either value is NaN, PositiveInfinity, or NegativeInfinity.
+    /// </remarks>
+    /// <param name="x">The 'X' value.</param>
+    /// <param name="y">The 'Y' value.</param>
     public Vector2(double x, double y)
     {
         AssertionHelper.InvalidDoubleCheck(x, nameof(x));
@@ -78,6 +114,13 @@ public class Vector2
         _y = y;
     }
 
+    /// <summary>
+    /// Copies the values of one Vector2 to another.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentNullException' if 'vec2' is null.
+    /// </remarks>
+    /// <param name="vec2">The existing Vector2 to duplicate.</param>
     public Vector2(Vector2 vec2)
     {
         AssertionHelper.NullCheck(vec2, nameof(vec2));
@@ -86,6 +129,14 @@ public class Vector2
         _y = vec2.Y;
     }
 
+    /// <summary>
+    /// Copies the values of one Vector3 to another.
+    /// </summary>
+    /// <remarks>
+    /// The 'Z' value of the Vector3 is ignored.
+    /// This will throw an 'ArgumentNullException' if 'vec3' is null.
+    /// </remarks>
+    /// <param name="vec2">The existing Vector3 to duplicate.</param>
     public Vector2(Vector3 vec3)
     {
         AssertionHelper.NullCheck(vec3, nameof(vec3));
@@ -94,6 +145,17 @@ public class Vector2
         _y = vec3.Y;
     }
 
+    /// <summary>
+    /// Override that compares the 'X' and 'Y' values of 2 Vector2s.
+    /// </summary>
+    /// <remarks>
+    /// The following outcomes are possible:
+    /// - If 'obj' is null, it'll return false.
+    /// - If 'obj' is this, it'll return true.
+    /// - Otherwise, the values are compared outright.
+    /// </remarks>
+    /// <param name="obj">The object to compare to this Vector2 instance.</param>
+    /// <returns>A boolean value if the object is equal to this Vector2 instance.</returns>
     public override bool Equals(object? obj)
     {
         if (ReferenceEquals(null, obj)) 
@@ -111,110 +173,10 @@ public class Vector2
                && MathHelper.Equals(Y, target.Y);
     }
 
+    /// <summary>
+    /// An override of ToString() that creates a string.
+    /// </summary>
+    /// <returns>A string containing the 'X' and 'Y' values.</returns>
     public override string ToString() => $"{{ x: {X}, y: {Y} }}";
-
-    // Operators
-    public static Vector2 operator -(Vector2 vector)
-    {
-        AssertionHelper.NullCheck(vector, nameof(vector));
-
-        return new Vector2(-vector.X, -vector.Y);
-    }
-    
-    public static Vector2 operator +(Vector2 left, double right)
-    {
-        AssertionHelper.NullCheck(left, nameof(left));
-        AssertionHelper.InvalidDoubleCheck(right, nameof(right));
-
-        return new Vector2(left.X + right, left.Y + right);
-    }
-
-    public static Vector2 operator -(Vector2 left, double right)
-    {
-        AssertionHelper.NullCheck(left, nameof(left));
-        AssertionHelper.InvalidDoubleCheck(right, nameof(right));
-
-        return new Vector2(left.X - right, left.Y - right);
-    }
-
-    public static Vector2 operator +(double left, Vector2 right)
-    {
-        AssertionHelper.InvalidDoubleCheck(left, nameof(left));
-        AssertionHelper.NullCheck(right, nameof(right));
-
-        return new Vector2(left + right.X, left + right.Y);
-    }
-
-    public static Vector2 operator -(double left, Vector2 right)
-    {
-        AssertionHelper.InvalidDoubleCheck(left, nameof(left));
-        AssertionHelper.NullCheck(right, nameof(right));
-
-        return new Vector2(left - right.X, left - right.Y);
-    }
-
-    public static Vector2 operator +(Vector2 left, Vector2 right)
-    {
-        AssertionHelper.NullCheck(left, nameof(left));
-        AssertionHelper.NullCheck(right, nameof(right));
-
-        return new Vector2(left.X + right.X, left.Y + right.Y);
-    }
-
-    public static Vector2 operator -(Vector2 left, Vector2 right)
-    {
-        AssertionHelper.NullCheck(left, nameof(left));
-        AssertionHelper.NullCheck(right, nameof(right));
-
-        return new Vector2(left.X - right.X, left.Y - right.Y);
-    }
-
-    public static Vector2 operator *(Vector2 left, Vector2 right)
-    {
-        AssertionHelper.NullCheck(left, nameof(left));
-        AssertionHelper.NullCheck(right, nameof(right));
-
-        return new Vector2(left.X * right.X, left.Y * right.Y);
-    }
-
-    public static Vector2 operator /(Vector2 left, Vector2 right)
-    {
-        AssertionHelper.NullCheck(left, nameof(left));
-        AssertionHelper.NullCheck(right, nameof(right));
-
-        return new Vector2(left.X / right.X, left.Y / right.Y);
-    }
-
-    public static Vector2 operator *(Vector2 vector, double value)
-    {
-        AssertionHelper.NullCheck(vector, nameof(vector));
-        AssertionHelper.InvalidDoubleCheck(value, nameof(value));
-
-        return new Vector2(vector.X * value, vector.Y * value);
-    }
-
-    public static Vector2 operator /(Vector2 vector, double value)
-    {
-        AssertionHelper.NullCheck(vector, nameof(vector));
-        AssertionHelper.InvalidDoubleCheck(value, nameof(value));
-
-        return new Vector2(vector.X / value, vector.Y / value);
-    }
-
-    public static Vector2 operator *(double value, Vector2 vector)
-    {
-        AssertionHelper.InvalidDoubleCheck(value, nameof(value));
-        AssertionHelper.NullCheck(vector, nameof(vector));
-
-        return new Vector2(value * vector.X, value * vector.Y);
-    }
-
-    public static Vector2 operator /(double value, Vector2 vector)
-    {
-        AssertionHelper.InvalidDoubleCheck(value, nameof(value));
-        AssertionHelper.NullCheck(vector, nameof(vector));
-
-        return new Vector2(value / vector.X, value / vector.Y);
-    }
 }
 /** @} */
---
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Data/Vector2Static.cs
@@ -0,0 +1,259 @@
+using Khartyko.InsigniaCreator.Library.Utility.Helpers;
+namespace Khartyko.InsigniaCreator.Library.Data;
+
+public partial class Vector2
+{
+    /// <summary>
+    /// Vector2 with 'X' and 'Y' values of 0.0.
+    /// </summary>
+    public static readonly Vector2 Zero = new(0, 0);
+    /// <summary>
+    /// Vector2 with 'X' and 'Y' values of 1.0.
+    /// </summary>
+    public static readonly Vector2 One = new(1, 1);
+
+    /// <summary>
+    /// Operator that negates a Vector2, making both values change their sign
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentNullException' if 'vector' is null
+    /// </remarks>
+    /// <param name="vector">The Vector2 to negate</param>
+    /// <returns>A Vector2 that has negated values from the operand.</returns>
+    public static Vector2 operator -(Vector2 vector)
+    {
+        AssertionHelper.NullCheck(vector, nameof(vector));
+
+        return new Vector2(-vector.X, -vector.Y);
+    }
+    
+    /// <summary>
+    /// Operator that adds both values of a Vector2 to a double and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentException: If 'left' is either NaN, PositiveInfinity, or NegativeInfinity.
+    /// - ArgumentNullException: If 'right' is null.
+    ///
+    /// This is functionally the same as Vector2 + double.
+    /// </remarks>
+    /// <param name="left">The first double operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the addition results from both operands.</returns>
+    public static Vector2 operator +(double left, Vector2 right)
+    {
+        AssertionHelper.InvalidDoubleCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left + right.X, left + right.Y);
+    }
+
+    /// <summary>
+    /// Operator that adds a double to both values of a Vector2 and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentNullException: If 'left' is null
+    /// - ArgumentException: If 'right' is either NaN, PositiveInfinity, or NegativeInfinity
+    ///
+    /// This is functionally the same as double + Vector2.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second double operand.</param>
+    /// <returns>A Vector2 with the addition results from both operands.</returns>
+    public static Vector2 operator +(Vector2 left, double right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.InvalidDoubleCheck(right, nameof(right));
+
+        return new Vector2(left.X + right, left.Y + right);
+    }
+
+    /// <summary>
+    /// Operator that adds 2 Vector2 instances and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentNullException' if 'left' or 'right' is null.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the addition results from both operands.</returns>
+    public static Vector2 operator +(Vector2 left, Vector2 right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left.X + right.X, left.Y + right.Y);
+    }
+
+    /// <summary>
+    /// Operator that subtracts both values of a Vector2 from a double and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentException: If 'left' is either NaN, PositiveInfinity, or NegativeInfinity.
+    /// - ArgumentNullException: If 'right' is null.
+    ///
+    /// This is functionally the same as 
+    /// </remarks>
+    /// <param name="left">The first double operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the subtraction results from both operands.</returns>
+    public static Vector2 operator -(double left, Vector2 right)
+    {
+        AssertionHelper.InvalidDoubleCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left - right.X, left - right.Y);
+    }
+
+    /// <summary>
+    /// Operator that subtracts a double from both values of a Vector2 and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentNullException: If 'left' is null.
+    /// - ArgumentException: If 'right' is either NaN, PositiveInfinity, or NegativeInfinity.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second double operand.</param>
+    /// <returns>A Vector2 with the subtraction results from both operands.</returns>
+    public static Vector2 operator -(Vector2 left, double right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.InvalidDoubleCheck(right, nameof(right));
+
+        return new Vector2(left.X - right, left.Y - right);
+    }
+
+    /// <summary>
+    /// Operator that subtracts one Vector2 instance from another and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentNullException' if 'left' or 'right' is null.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the subtraction results from both operands.</returns>
+    public static Vector2 operator -(Vector2 left, Vector2 right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left.X - right.X, left.Y - right.Y);
+    }
+
+    /// <summary>
+    /// Operator that multiplies both values of a Vector2 to a double and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentException: If 'value' is either NaN, PositiveInfinity, or NegativeInfinity.
+    /// - ArgumentNullException: If 'vector' is null.
+    ///
+    /// This is functionally the same as Vector2 * double.
+    /// </remarks>
+    /// <param name="left">The first double operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the multiplication results from both operands.</returns>
+    public static Vector2 operator *(double left, Vector2 right)
+    {
+        AssertionHelper.InvalidDoubleCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left * right.X, left * right.Y);
+    }
+
+    /// <summary>
+    /// Operator that multiplies a double to both values of a Vector2 and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentNullException: If 'left' is null.
+    /// - ArgumentException: If 'right' is either NaN, PositiveInfinity, or NegativeInfinity.
+    ///
+    /// This is functionally the same as Vector2 * double.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second double operand.</param>
+    /// <returns>A Vector2 with the multiplication results from both operands.</returns>
+    public static Vector2 operator *(Vector2 left, double right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.InvalidDoubleCheck(right, nameof(right));
+
+        return new Vector2(left.X * right, left.Y * right);
+    }
+
+    /// <summary>
+    /// Operator that multiplies 2 Vector2 instances and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentNullException' if 'left' or 'right' is null.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the multiplication results from both operands.</returns>
+    public static Vector2 operator *(Vector2 left, Vector2 right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left.X * right.X, left.Y * right.Y);
+    }
+
+    /// <summary>
+    /// Operator that divides a double by both values of a Vector2 and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentException: If 'left' is either NaN, PositiveInfinity, or NegativeInfinity.
+    /// - ArgumentNullException: If 'right' is null.
+    /// </remarks>
+    /// <param name="left">The first double operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the division results from both operands.</returns>
+    public static Vector2 operator /(double left, Vector2 right)
+    {
+        AssertionHelper.InvalidDoubleCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left / right.X, left / right.Y);
+    }
+
+    /// <summary>
+    /// Operator that divides both values of a Vector2 by a double and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw the following exceptions:
+    /// - ArgumentNullException: If 'left' is null.
+    /// - ArgumentException: If 'right' is either NaN, PositiveInfinity, or NegativeInfinity.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second double operand.</param>
+    /// <returns>A Vector2 with the division results from both operands.</returns>
+    public static Vector2 operator /(Vector2 left, double right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.InvalidDoubleCheck(right, nameof(right));
+
+        return new Vector2(left.X / right, left.Y / right);
+    }
+
+    /// <summary>
+    /// Operator that divides one Vector2 instance from another and returns the results as a Vector2.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentNullException' if 'left' or 'right' is null.
+    /// </remarks>
+    /// <param name="left">The first Vector2 operand.</param>
+    /// <param name="right">The second Vector2 operand.</param>
+    /// <returns>A Vector2 with the division results from both operands.</returns>
+    public static Vector2 operator /(Vector2 left, Vector2 right)
+    {
+        AssertionHelper.NullCheck(left, nameof(left));
+        AssertionHelper.NullCheck(right, nameof(right));
+
+        return new Vector2(left.X / right.X, left.Y / right.Y);
+    }
+}
