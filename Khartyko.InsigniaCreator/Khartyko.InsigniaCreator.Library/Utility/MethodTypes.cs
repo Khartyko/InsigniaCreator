a3c244dc481c56c355e49ad7e6e4b70d1a740ccd
Damien Bevins <damien.bevins@outlook.com>
2023-08-28 02:48:00 -0500

    Add more documentation

--- Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Utility/MethodTypes.cs
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Utility/MethodTypes.cs
@@ -3,14 +3,44 @@
  */
 namespace Khartyko.InsigniaCreator.Library.Utility;
 
+/// <summary>
+/// Enum that represents the different kinds of method types that can be associated with Reflection.
+/// </summary>
 public enum MethodTypes
 {
+    /// <summary>
+    /// Represents the 'get' portion of a property.
+    /// </summary>
     PropertySet,
+    
+    /// <summary>
+    /// Represents the 'set' portion of a property.
+    /// </summary>
     PropertyGet,
+    
+    /// <summary>
+    /// Represents the 'get' portion of an indexing operator.
+    /// </summary>
     IndexerGet,
+    
+    /// <summary>
+    /// Represents the 'set' portion of an indexing operator.
+    /// </summary>
     IndexerSet,
+    
+    /// <summary>
+    /// Represents a constructor or ".ctor".
+    /// </summary>
     Constructor,
+    
+    /// <summary>
+    /// Represents a Lambda/Anonymous method.
+    /// </summary>
     Lambda,
+    
+    /// <summary>
+    /// Represents any other method that is created.
+    /// </summary>
     RegularMethod
 }
 /** @} */
--- Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Utility/ReflectionMetadata.cs
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Utility/ReflectionMetadata.cs
@@ -5,8 +5,20 @@ using System.Reflection;
 
 namespace Khartyko.InsigniaCreator.Library.Utility;
 
+/// <summary>
+/// Class that holds data that describes a method within C#.
+/// </summary>
+/// <remarks>
+/// This is primarily intended to be used with logging and debugging.
+/// </remarks>
 public class ReflectionMetadata
 {
+    /// <summary>
+    /// Constructs an object with the given data about a method.
+    /// </summary>
+    /// <param name="type">The type of the class that declares the method.</param>
+    /// <param name="methodBase">The method data used in Reflectino.</param>
+    /// <param name="methodType">The type of method.</param>
     public ReflectionMetadata(Type type, MethodBase methodBase, MethodTypes methodType)
     {
         Type = type;
@@ -14,8 +26,19 @@ public class ReflectionMetadata
         MethodType = methodType;
     }
 
-    public Type Type { get; set; }
-    public MethodBase MethodBase { get; set; }
-    public MethodTypes MethodType { get; set; }
+    /// <summary>
+    /// The declaring Type that has the method.
+    /// </summary>
+    public Type Type { get; }
+    
+    /// <summary>
+    /// The method info that is retrieved.
+    /// </summary>
+    public MethodBase MethodBase { get; }
+    
+    /// <summary>
+    /// The type of method that this class describes.
+    /// </summary>
+    public MethodTypes MethodType { get; }
 }
 /** @} */
