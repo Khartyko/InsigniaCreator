6105a13a589877ff301e8b41b91a879d2cb6bab9
Damien Bevins <damien.bevins@outlook.com>
2023-08-28 03:04:20 -0500

    Add more documentation

--- Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Utility/Helpers/ReflectionHelper.cs
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Library/Utility/Helpers/ReflectionHelper.cs
@@ -7,11 +7,26 @@ using System.Text.RegularExpressions;
 
 namespace Khartyko.InsigniaCreator.Library.Utility.Helpers;
 
+/// <summary>
+/// Static class that has methods that aid in getting a method's data or signature
+/// </summary>
 public static partial class ReflectionHelper
 {
+    /// <summary>
+    /// Regex that is used to verify a method's 'Name' denotes a lambda method.
+    /// </summary>
+    /// <returns>A Regex object with the provided pattern.</returns>
     [GeneratedRegex("<[\\w_]+>b__[0-9]+")]
     private static partial Regex LambdaRegex();
 
+    /// <summary>
+    /// Gets the metadata of any methods that call this method, depending on how many frames upstream are specified.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentOutOfRangeException' if 'frameOffset' is negative.
+    /// </remarks>
+    /// <param name="frameOffset">How many method calls to check upstream.</param>
+    /// <returns>Metadata in regards to a calling method.</returns>
     public static ReflectionMetadata GetCallerMetadata(int frameOffset = 0)
     {
         AssertionHelper.PositiveCheck(frameOffset, nameof(frameOffset));
@@ -58,6 +73,16 @@ public static partial class ReflectionHelper
         return new ReflectionMetadata(ownerType, methodBase, methodType);
     }
 
+    /// <summary>
+    /// Creates the signature of a method, and denotes a particular parameter in its list, if found.
+    /// </summary>
+    /// <remarks>
+    /// This will throw an 'ArgumentNullException' if 'reflectionMetadata' is null.
+    /// 'parameterName' can be null, empty, or anything else, as it is only used to look for a parameter's name.
+    /// </remarks>
+    /// <param name="reflectionMetadata">The metadata of the method.</param>
+    /// <param name="parameterName">A parameter that may be found in the method's parameter list.</param>
+    /// <returns>A string that holds the signature of the described method.</returns>
     public static string ConstructMethodSignature(ReflectionMetadata reflectionMetadata, string? parameterName = null)
     {
         AssertionHelper.NullCheck(reflectionMetadata, nameof(reflectionMetadata));
