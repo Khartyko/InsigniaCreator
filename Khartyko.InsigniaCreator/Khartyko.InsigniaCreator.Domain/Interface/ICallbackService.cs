b32a8e2cae773aa37cfee9072518c3f24c45b054
Damien Bevins <damien.bevins@outlook.com>
2023-09-02 08:55:14 -0500

    Add a Callback service that can be used to register and unregister callbacks

---
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Domain/Interface/ICallbackService.cs
@@ -0,0 +1,19 @@
+﻿/** \addtogroup Domain
+* @{
+*/
+
+using Khartyko.InsigniaCreator.Domain.Utility;
+using Khartyko.InsigniaCreator.Library.Entity;
+using Khartyko.InsigniaCreator.Library.Interfaces;
+
+namespace Khartyko.InsigniaCreator.Domain.Interface;
+
+public interface ICallbackService<TEntity>
+    where TEntity : IEntity
+{
+    void RegisterCallback(Action<Cartograph?> callback, CallbackType callbackType);
+    void RegisterCallback(Action<Cartograph?> callback, params CallbackType[] callbackTypes);
+    void RemoveCallback(Action<Cartograph?> callback, CallbackType callbackType);
+}
+
+/** @} */
