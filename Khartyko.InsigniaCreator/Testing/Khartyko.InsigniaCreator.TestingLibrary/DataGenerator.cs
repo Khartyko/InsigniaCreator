da16c30b0645c7609e9b7c370d322da59785d3c5
Damien Bevins <damien.bevins@outlook.com>
2023-09-05 08:50:04 -0500

    Add static helper method for generating ulongs

--- Khartyko.InsigniaCreator/Testing/Khartyko.InsigniaCreator.TestingLibrary/DataGenerator.cs
+++ Khartyko.InsigniaCreator/Testing/Khartyko.InsigniaCreator.TestingLibrary/DataGenerator.cs
@@ -9,6 +9,7 @@ public static class DataGenerator
 
     public static int GenerateRandomInt(int min, int max) => s_random.Next(min, max);
     public static long GenerateRandomLong(long min, long max) => s_random.NextInt64(min, max);
+    public static ulong GenerateRandomULong(long max) => (ulong)s_random.NextInt64(0, max);
     public static double GenerateRandomDouble(double multiplier = 100) => s_random.NextDouble() * multiplier;
 
     public static Vector2 GenerateRandomVector2() => new(
