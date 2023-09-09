07e5a13e1cc3f96c036d42b38da088de71113083
Damien Bevins <damien.bevins@outlook.com>
2023-09-09 03:36:50 -0500

    Add tests for NodeData.

---
+++ Khartyko.InsigniaCreator/Testing/Khartyko.InsigniaCreator.Domain.Testing/Data/NodeDataTests.cs
@@ -0,0 +1,31 @@
+﻿using Khartyko.InsigniaCreator.Domain.Data;
+using Khartyko.InsigniaCreator.Library.Data;
+using Khartyko.InsigniaCreator.TestingLibrary;
+namespace Khartyko.InsigniaCreator.Domain.Testing.Data;
+
+public class NodeDataTests
+{
+	[Fact]
+	public void Position_Succeeds()
+	{
+		Vector2 position = DataGenerator.GenerateRandomVector2();
+		
+		var data = new NodeData
+		{
+			Position = position
+		};
+		
+		Assert.Equal(position, data.Position);
+	}
+
+	[Fact]
+	public void Position_Set_Null_Succeeds()
+	{
+		var data = new NodeData
+		{
+			Position = null
+		};
+		
+		Assert.Null(data.Position);
+	}
+}
