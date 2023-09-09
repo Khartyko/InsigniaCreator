53ce745573a75708cc6d78c7e53ad8fd2469b0b4
Damien Bevins <damien.bevins@outlook.com>
2023-09-09 11:02:38 -0500

    Add INetworkGenerator interface

---
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Domain/Interfaces/INetworkGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Khartyko.InsigniaCreator.Domain.Data;
+using Khartyko.InsigniaCreator.Library.Entity;
+
+namespace Khartyko.InsigniaCreator.Domain.Interface;
+
+/// <summary>
+/// Declares a method that generates a TemplateNetwork with data of the specified type.
+/// </summary>
+/// <typeparam name="TGenerationData">The specified type of TemplateNetwork to generate.</typeparam>
+public interface INetworkGenerator<TGenerationData>
+    where TGenerationData : NetworkData
+{
+    /// <summary>
+    /// Generates a TemplateNetwork with the given data.
+    /// </summary>
+    /// <param name="generationData">The data used in generating a TemplateNetwork.</param>
+    /// <returns>The newly generated TemplateNetwork.</returns>
+    TemplateNetwork GenerateNetwork(TGenerationData generationData);
+}
