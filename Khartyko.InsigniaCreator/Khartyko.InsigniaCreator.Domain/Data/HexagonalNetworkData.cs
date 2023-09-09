d226d7d6f5346bfd284890891d5c0a0d049be784
Damien Bevins <damien.bevins@outlook.com>
2023-09-09 11:25:07 -0500

    Add Triangular and Hexagonal NetworkData along with the necessary tests

---
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Domain/Data/HexagonalNetworkData.cs
@@ -0,0 +1,18 @@
+﻿/** \addtogroup Domain
+* @{
+*/
+
+namespace Khartyko.InsigniaCreator.Domain.Data;
+
+/// <summary>
+/// Represents the data for a TemplateNetwork with a hexagonal tessellating grid.
+/// </summary>
+public class HexagonalNetworkData : NetworkData
+{
+    /// <summary>
+    /// Gets or sets whether the triangles will start with an offset hexagon or not.
+    /// </summary>
+    public bool? StartOffset { get; set; }
+}
+
+/** @} */
---
+++ Khartyko.InsigniaCreator/Testing/Khartyko.InsigniaCreator.Domain.Testing/Data/TriangularNetworkDataTests.cs
@@ -0,0 +1,61 @@
+﻿using Khartyko.InsigniaCreator.Domain.Data;
+using Khartyko.InsigniaCreator.TestingLibrary;
+
+namespace Khartyko.InsigniaCreator.Domain.Testing.Data;
+
+// TODO: Fill out these
+
+public class TriangularNetworkDataTests
+{
+	[Fact]
+	public void StartFlipped_ValidValue_Succeeds()
+	{
+		bool value = DataGenerator.GenerateRandomBool();
+
+		var data = new TriangularNetworkData
+		{
+			StartFlipped = value
+		};
+		
+		Assert.Equal(value, data.StartFlipped);
+	}
+
+	[Fact]
+	public void StartFlipped_Null_Succeeds()
+	{
+		var data = new TriangularNetworkData();
+		
+		Assert.Null(data.StartFlipped);
+	}
+
+	[Fact]
+	public void StartFlipped_NullToValidValue_Succeeds()
+	{
+		var data = new TriangularNetworkData();
+		
+		Assert.Null(data.StartFlipped);
+		
+		bool value = DataGenerator.GenerateRandomBool();
+
+		data.StartFlipped = value;
+		
+		Assert.Equal(value, data.StartFlipped);
+	}
+
+	[Fact]
+	public void StartFlipped_ValidValueToNull_Succeeds()
+	{
+		bool value = DataGenerator.GenerateRandomBool();
+
+		var data = new TriangularNetworkData
+		{
+			StartFlipped = value
+		};
+		
+		Assert.Equal(value, data.StartFlipped);
+
+		data.StartFlipped = null;
+		
+		Assert.Null(data.StartFlipped);
+	}
+}
