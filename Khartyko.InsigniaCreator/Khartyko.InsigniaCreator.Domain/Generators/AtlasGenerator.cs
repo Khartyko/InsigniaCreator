e056a321795464e6feefba2f6553dd9c24dd880b
Damien Bevins <damien.bevins@outlook.com>
2023-11-11 12:39:06 -0700

    Add implementation and tests for AtlasGenerator.Generate

---
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Domain/Generators/AtlasGenerator.cs
@@ -0,0 +1,68 @@
+﻿/** \addtogroup Khartyko.InsigniaCreator.Domain
+ * @{
+ */
+
+using Khartyko.InsigniaCreator.Domain.Data;
+using Khartyko.InsigniaCreator.Domain.Interfaces;
+using Khartyko.InsigniaCreator.Library.Entity;
+using Khartyko.InsigniaCreator.Library.Utility.Helpers;
+
+namespace Khartyko.InsigniaCreator.Domain.Generators;
+
+/// <summary>
+/// Implementation of an IGenerator for Cartographs and CartographDatas.
+/// </summary>
+public class AtlasGenerator : IGenerator<AtlasData, Atlas>
+{
+	private ulong _currentId = 1L;
+    
+	/// <summary>
+	/// Generates a Cartograph with the given data, and supplies an id.
+	/// </summary>
+	/// <param name="data">The data to use to generate a Cartograph.</param>
+	/// <exception cref="NullReferenceException">Can be thrown if 'data' is null.</exception>
+	/// <returns>A newly generated Cartograph.</returns>
+    public Atlas Generate(AtlasData data)
+    {
+	    AssertionHelper.NullCheck(data, nameof(data));
+	    AssertionHelper.EmptyOrWhitespaceCheck(data.Name, "data::Name");
+	    /*
+	     * TODO: Enforce validations:
+	     * - Cannot be less than some value
+	     * - Cannot be greater than some value
+	     *
+	     * Note: Above values are yet to be determined
+	     */
+	    AssertionHelper.PositiveCheck(data.Width, "data::Width");
+	    AssertionHelper.PositiveCheck(data.Height, "data::Height");
+	    AssertionHelper.NullCheck(data.Background, "data::Background");
+
+	    if (data.Cartographs is not null && data.Cartograph is not null)
+	    {
+		    var cartographs = new List<Cartograph>(data.Cartographs);
+
+		    List<ulong> ids = data.Cartographs.Select(cartograph => cartograph.Id).ToList();
+
+		    if (!ids.Contains(data.Cartograph.Id))
+		    {
+			    cartographs.Add(data.Cartograph);
+		    }
+		    
+		    return new Atlas(_currentId++, data.Name, data.Width, data.Height, data.Background, cartographs);
+	    }
+	    
+	    if (data.Cartographs is not null && data.Cartograph is null)
+	    {
+		    return new Atlas(_currentId++, data.Name, data.Width, data.Height, data.Background, data.Cartographs);
+	    }
+
+	    if (data.Cartographs is null && data.Cartograph is not null)
+	    {
+		    return new Atlas(_currentId++, data.Name, data.Width, data.Height, data.Background, data.Cartograph); 
+	    }
+	    
+	    return new Atlas(_currentId++, data.Name, data.Width, data.Height, data.Background);
+    }
+}
+
+/** @} */
---
+++ Khartyko.InsigniaCreator/Testing/Khartyko.InsigniaCreator.Domain.Testing/Generators/AtlasGeneratorTests.cs
@@ -0,0 +1,326 @@
+﻿
+using Khartyko.InsigniaCreator.Domain.Data;
+using Khartyko.InsigniaCreator.Domain.Generators;
+using Khartyko.InsigniaCreator.Library.Data;
+using Khartyko.InsigniaCreator.Library.Entity;
+using Khartyko.InsigniaCreator.TestingLibrary;
+namespace Khartyko.InsigniaCreator.Domain.Testing.Generators;
+
+public class AtlasGeneratorTests
+{
+	[Fact]
+	public void Generate_Valid_NeitherCartograph_Succeeds()
+	{
+		var data = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = 1280,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255)
+		};
+		
+		var generator = new AtlasGenerator();
+
+		Atlas atlas = generator.Generate(data);
+		Assert.NotNull(atlas);
+		Assert.Equal(data.Name, atlas.Name);
+		Assert.Equal(data.Width, atlas.Width);
+		Assert.Equal(data.Height, atlas.Height);
+		Assert.Equal(data.Background, atlas.BackgroundColor);
+		Assert.Empty(atlas.Cartographs);
+	}
+
+	[Fact]
+	public void Generate_Valid_CartographOnly_Succeeds()
+	{
+		var cartographData = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		
+		var cartographGenerator = new CartographGenerator();
+
+		Cartograph cartograph = cartographGenerator.Generate(cartographData);
+		
+		var atlasData = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = 1280,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255),
+			Cartograph = cartograph
+		};
+		
+		var atlasGenerator = new AtlasGenerator();
+
+		Atlas atlas = atlasGenerator.Generate(atlasData);
+		Assert.NotNull(atlas);
+		Assert.Equal(atlasData.Name, atlas.Name);
+		Assert.Equal(atlasData.Width, atlas.Width);
+		Assert.Equal(atlasData.Height, atlas.Height);
+		Assert.Equal(atlasData.Background, atlas.BackgroundColor);
+		Assert.Equal(1, atlas.Cartographs.Count);
+		Assert.Equal(cartograph, atlas.Cartographs.First());
+	}
+
+	[Fact]
+	public void Generate_Valid_CartographsOnly_Succeeds()
+	{
+		var cartographData1 = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph I",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		var cartographData2 = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph II",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		
+		var cartographGenerator = new CartographGenerator();
+
+		Cartograph cartograph1 = cartographGenerator.Generate(cartographData1);
+		Cartograph cartograph2 = cartographGenerator.Generate(cartographData2);
+		
+		var atlasData = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = 1280,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255),
+			Cartographs = new List<Cartograph>
+			{
+				cartograph1,
+				cartograph2
+			}
+		};
+		
+		var atlasGenerator = new AtlasGenerator();
+
+		Atlas atlas = atlasGenerator.Generate(atlasData);
+		Assert.NotNull(atlas);
+		Assert.Equal(atlasData.Name, atlas.Name);
+		Assert.Equal(atlasData.Width, atlas.Width);
+		Assert.Equal(atlasData.Height, atlas.Height);
+		Assert.Equal(atlasData.Background, atlas.BackgroundColor);
+		Assert.Equal(2, atlas.Cartographs.Count);
+		Assert.Contains(atlasData.Cartographs[0], atlas.Cartographs);
+		Assert.Contains(atlasData.Cartographs[1], atlas.Cartographs);
+	}
+
+	[Fact]
+	public void Generate_Valid_BothWithNoOverlap_Succeeds()
+	{
+		var cartographData1 = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph I",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		var cartographData2 = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph II",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		var cartographData3 = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph III",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		
+		var cartographGenerator = new CartographGenerator();
+
+		Cartograph cartograph1 = cartographGenerator.Generate(cartographData1);
+		Cartograph cartograph2 = cartographGenerator.Generate(cartographData2);
+		Cartograph cartograph3 = cartographGenerator.Generate(cartographData3);
+		
+		var atlasData = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = 1280,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255),
+			Cartograph = cartograph3,
+			Cartographs = new List<Cartograph>
+			{
+				cartograph1,
+				cartograph2
+			}
+		};
+		
+		var atlasGenerator = new AtlasGenerator();
+
+		Atlas atlas = atlasGenerator.Generate(atlasData);
+		Assert.NotNull(atlas);
+		Assert.Equal(atlasData.Name, atlas.Name);
+		Assert.Equal(atlasData.Width, atlas.Width);
+		Assert.Equal(atlasData.Height, atlas.Height);
+		Assert.Equal(atlasData.Background, atlas.BackgroundColor);
+		Assert.Equal(3, atlas.Cartographs.Count);
+		Assert.Contains(atlasData.Cartograph, atlas.Cartographs);
+		Assert.Contains(atlasData.Cartographs[0], atlas.Cartographs);
+		Assert.Contains(atlasData.Cartographs[1], atlas.Cartographs);
+	}
+
+	[Fact]
+	public void Generate_Valid_BothWithOverlap_Succeeds()
+	{
+		var cartographData1 = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph I",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		var cartographData2 = new CartographData
+		{
+			AtlasId = 1L,
+			Name = "Cartograph II",
+			Network = DataGenerator.GenerateSquareNetwork()
+		};
+		
+		var cartographGenerator = new CartographGenerator();
+
+		Cartograph cartograph1 = cartographGenerator.Generate(cartographData1);
+		Cartograph cartograph2 = cartographGenerator.Generate(cartographData2);
+		
+		var atlasData = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = 1280,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255),
+			Cartograph = cartograph1,
+			Cartographs = new List<Cartograph>
+			{
+				cartograph1,
+				cartograph2
+			}
+		};
+		
+		var atlasGenerator = new AtlasGenerator();
+
+		Atlas atlas = atlasGenerator.Generate(atlasData);
+		Assert.NotNull(atlas);
+		Assert.Equal(atlasData.Name, atlas.Name);
+		Assert.Equal(atlasData.Width, atlas.Width);
+		Assert.Equal(atlasData.Height, atlas.Height);
+		Assert.Equal(atlasData.Background, atlas.BackgroundColor);
+		Assert.Equal(2, atlas.Cartographs.Count);
+		Assert.Contains(atlasData.Cartographs[0], atlas.Cartographs);
+		Assert.Contains(atlasData.Cartographs[1], atlas.Cartographs);
+	}
+
+	[Fact]
+	public void Generate_NullName_Succeeds()
+	{
+		var data = new AtlasData
+		{
+			Width = 1280,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255)
+		};
+		
+		var generator = new AtlasGenerator();
+
+		Assert.Throws<ArgumentNullException>(() => generator.Generate(data));
+	}
+
+	[Theory]
+	[InlineData("")]
+	[InlineData(" ")]
+	[InlineData("\r\t\n")]
+	public void Generate_InvalidName_Succeeds(string name)
+	{
+		var data = new AtlasData
+		{
+			Name = name,
+			Width = 1280,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255)
+		};
+		
+		var generator = new AtlasGenerator();
+
+		Assert.Throws<ArgumentException>(() => generator.Generate(data));
+	}
+
+	[Theory]
+	[InlineData(-1)]
+	[InlineData(0)]
+	public void Generate_WidthOutOfRange_Succeeds(double width)
+	{
+		var data = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = width,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255)
+		};
+		
+		var generator = new AtlasGenerator();
+
+		Assert.Throws<ArgumentOutOfRangeException>(() => generator.Generate(data));
+	}
+
+	[Theory]
+	[InlineData(double.PositiveInfinity)]
+	[InlineData(double.NaN)]
+	[InlineData(double.NegativeInfinity)]
+	public void Generate_InvalidWidth_Succeeds(double width)
+	{
+		var data = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = width,
+			Height = 800,
+			Background = new RgbColor(255, 255, 255)
+		};
+		
+		var generator = new AtlasGenerator();
+
+		Assert.Throws<ArgumentException>(() => generator.Generate(data));
+	}
+
+	[Theory]
+	[InlineData(-1)]
+	[InlineData(0)]
+	public void Generate_HeightOutOfRange_Succeeds(double height)
+	{
+		var data = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = 1280,
+			Height = height,
+			Background = new RgbColor(255, 255, 255)
+		};
+		
+		var generator = new AtlasGenerator();
+
+		Assert.Throws<ArgumentOutOfRangeException>(() => generator.Generate(data));
+	}
+
+	[Theory]
+	[InlineData(double.PositiveInfinity)]
+	[InlineData(double.NaN)]
+	[InlineData(double.NegativeInfinity)]
+	public void Generate_InvalidHeight_Succeeds(double height)
+	{
+		var data = new AtlasData
+		{
+			Name = "Atlas I",
+			Width = 1280,
+			Height = height,
+			Background = new RgbColor(255, 255, 255)
+		};
+		
+		var generator = new AtlasGenerator();
+
+		Assert.Throws<ArgumentException>(() => generator.Generate(data));
+	}
+}
