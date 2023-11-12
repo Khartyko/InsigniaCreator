336d41fd7dd81cada27abe37ec9562fbd55b428d
Damien Bevins <damien.bevins@outlook.com>
2023-11-12 11:39:35 -0700

    Add base abstraction for INetworkGenerator

--- Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Domain/Interfaces/INetworkGenerator.cs
+++ Khartyko.InsigniaCreator/Khartyko.InsigniaCreator.Domain/Interfaces/INetworkGenerator.cs
@@ -8,10 +8,17 @@ using Khartyko.InsigniaCreator.Library.Entity;
 namespace Khartyko.InsigniaCreator.Domain.Interfaces;
 
 /// <summary>
+/// This abstracts the type away so it can be used in a more general sense.
+/// </summary>
+public interface INetworkGenerator
+{
+}
+
+/// <summary>
 /// Declares a method that generates a TemplateNetwork with data of the specified type.
 /// </summary>
 /// <typeparam name="TGenerationData">The specified type of TemplateNetwork to generate.</typeparam>
-public interface INetworkGenerator<TGenerationData>
+public interface INetworkGenerator<in TGenerationData> : INetworkGenerator
     where TGenerationData : NetworkData
 {
     /// <summary>
